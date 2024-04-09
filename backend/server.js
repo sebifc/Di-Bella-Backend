81443babb9ca2193a1f1456e6f88da5d29f4fc87
sebifc <45524493+sebifc@users.noreply.github.com>
2024-04-09 20:02:12 +0200

    fix url

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://gestiondibella.vercell.app"],
+    origin: ["http://localhost:3000", "https://gestiondibella.vercel.app"],
     credentials: true,
   })
 );
